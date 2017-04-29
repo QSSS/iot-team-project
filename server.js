b153cf977694ee8508ca4d45235da00209ddbdfe
QSSS <zqs901103@gmail.com>
2017-04-28 17:14:16 -0700

    api

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 var express = require('express')
   app = express(),
-  config = require('./config');
+  config = require('./config'),
+  bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json());
 
 //Recieve image data in JSON Format
 app.post('/imageData', function(req, res) {
+  if (!req.body) return res.sendStatus(400);
 
 });
+
+app.post('/mobile/imageData', function(req, res) {
+
+})
 
 app.listen(config.port);
 
