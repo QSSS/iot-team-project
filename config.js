321deb9ce23c2018db6d11ab7665c4aef713c415
GIV <giyyanan@andrew.cmu.edu>
2017-04-28 17:06:23 -0700

    firebase url added

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 var path = require('path');
-
 var rootPath = path.normalize(__dirname);
 
 module.exports = {
   root: rootPath,
   port: process.env.PORT || 5001,
-  firebaseUrl: ""
+  firebase_config :{
+    apiKey: "AIzaSyCAYpc1ssJ06QMxKRcmZo-5Ow71UxO-bgE",
+    authDomain: "miot-team4.firebaseapp.com",
+    databaseURL: "https://miot-team4.firebaseio.com",
+    projectId: "miot-team4",
+    storageBucket: "miot-team4.appspot.com",
+    messagingSenderId: "573387262618"
+  }
 };
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var firebase = require('firebase')
 var express = require('express')
   app = express(),
   config = require('./config');
@@ -8,5 +9,7 @@ app.post('/imageData', function(req, res) {
 });
 
 app.listen(config.port);
+firebase.initializeApp(config.firebase_config);
+console.log(firebase.database())
 
 console.log('Server Up on: ' + config.port);
